7c707b92bdea4346aba9219527eb1dc301f4e3b0
Mingwei <mingwei.y@gmail.com>
2022-03-22 14:51:16 -0700

    PEN-1661: Add new scheduler to delete old event records

--- api/src/main/java/ca/bc/gov/educ/api/sld/SldApiResourceApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/sld/SldApiResourceApplication.java
@@ -1,14 +1,21 @@
 package ca.bc.gov.educ.api.sld;
 
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.PlatformTransactionManager;
 
 /**
  * The type Sld api resource application.
@@ -16,6 +23,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableCaching
+@EnableSchedulerLock(defaultLockAtMostFor = "1s")
 public class SldApiResourceApplication {
 
   /**
@@ -63,5 +71,11 @@ public class SldApiResourceApplication {
         .anyRequest().authenticated().and()
         .oauth2ResourceServer().jwt();
     }
+  }
+
+  @Bean
+  @Autowired
+  public LockProvider lockProvider(JdbcTemplate jdbcTemplate, PlatformTransactionManager transactionManager) {
+    return new JdbcTemplateLockProvider(jdbcTemplate, transactionManager, "SLD_SHEDLOCK");
   }
 }
--- api/src/main/java/ca/bc/gov/educ/api/sld/repository/EventRepository.java
+++ api/src/main/java/ca/bc/gov/educ/api/sld/repository/EventRepository.java
@@ -2,12 +2,21 @@ package ca.bc.gov.educ.api.sld.repository;
 
 import ca.bc.gov.educ.api.sld.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, UUID> {
   Optional<Event> findByEventTypeAndSagaId(String eventType, UUID sagaId);
+
+  @Transactional
+  @Modifying
+  @Query("delete from Event where createDate <= :createDate")
+  void deleteByCreateDateBefore(LocalDateTime createDate);
 }
